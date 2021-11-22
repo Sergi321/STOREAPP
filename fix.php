f18a72af853c86692a407201570cd512dcde275f
Sergi321 <73123084+Sergi321@users.noreply.github.com>
2021-11-22 12:41:49 +0100

    Add files via upload

--- fix.php
+++ fix.php
@@ -7,16 +7,19 @@ if ($_SESSION['username'] == null) {
     exit();
 }
 $username = $_SESSION['username'];
+
+
 ?>
 <!doctype html>
 <html lang="en">
+<link rel="stylesheet" href="../external/adduseradmin.css">
 
 <head>
     <title>Modify Page</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="../external/adduseradmin.css">
+
 
 </head>
 
@@ -26,35 +29,34 @@ $username = $_SESSION['username'];
         <div class="h">
             <div class="container">
                 <div class="title">
-                    <h2> <b>EDITAR USUARIOS | <?php echo $username ?></b></h2>
+                    <h2> <b>EDITAR PRODUCTOS | <?php echo $username ?></b></h2>
                 </div>
 
-                <form method="POST" action="controlador/fix.php">
-                    <div class="form-group">
-                        <label for="exampleInputEmail1"></label>
 
-                        <input type="text" class="form-control" name="username" placeholder="Usuario: " required>
-                        <input type="hidden" value="<?php echo $_GET['id'] ?>" name="id" />
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1"> </label>
-
-                        <input type="email" name="email" placeholder="Email: " required>
-
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1"> </label>
-
-                        <input type="text" name="password" placeholder="Contraseña: " required>
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1"> </label>
-
-                        <input type="text" name="usertype" placeholder="Rol: " required>
-                    </div>
+                <div class="fixproduct">
+                    <form method="POST" action="controlador/fix.php">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1"></label>
+                            <input type="text" class="form-control" name="name" placeholder="Producto : <?php echo $_GET['message']; ?>" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1"></label>
+                            <input type="number" class="form-control" name="value" placeholder="Stock : <?php echo $_GET['amount']; ?>" required>
+                            <input type="hidden" value="<?php echo $_GET['id'] ?>" name="id" />
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1"></label>
+                            <input type="float" class="form-control" name="price" placeholder="Precio : <?php echo $_GET['price']; ?>" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1"></label>
+                            <input  type="text" class="form-control" name="image" placeholder="<?php echo $_GET['image']; ?>" required>
+                        </div>
+                    
                     <input value="Guardar Cambios" type="submit">
-                    <a href="adminusercontrol.php"><input value="Volver al panel" type="button"></a>
-                </form>
+                    <a href="admincontrol.php"><input value="Volver al panel" type="button"></a>
+                    </form>
+                </div>
             </div>
         </div>
         <?php
