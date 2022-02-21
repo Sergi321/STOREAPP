cb3658368d38bc9262b092113870c218bfbecad8
Sergio Martín Cabello <73123084+Sergi321@users.noreply.github.com>
2022-02-21 10:38:30 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect("PMYSQL145.dns-servicio.com:3306","superuser","vGg~8d77","9072067_storeapp");
+    $con = mysqli_connect("your_server","your_user","your_password","your_db_table");
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
